118d8789eaf87ea343aa0bcd7caac8ad0e67fba1
fabiorrodrigues <fabio.rodrigues@camarajau.sp.gov.br>
2023-04-18 16:12:03 -0300

    pegar hora atual do banco3

--- src/main/java/com/example/fabio/brasileiroapi/model/repository/PalpiteRepository.java
+++ src/main/java/com/example/fabio/brasileiroapi/model/repository/PalpiteRepository.java
@@ -45,7 +45,7 @@ public interface PalpiteRepository extends JpaRepository<Palpite, Long> {
 			, nativeQuery = true)
 	Page<Ranking> buscarRanking(Pageable pageable);
 	
-	@Query(value = " SELECT now()", nativeQuery = true)
+	@Query(value = " SELECT now()- interval '1 hour'", nativeQuery = true)
 	Date buscarHoraAtual();
 	
 }
